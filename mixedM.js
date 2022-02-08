25ebb3e2b8c9ceab26c5787e0eee485878756d85
juanwallss <juancparedesg@gmail.com>
2022-02-07 18:13:03 -0800

    bobs idea

--- mixedM.js
+++ mixedM.js
@@ -1,32 +1,12 @@
-let inspirationalPhrases = ["When you have a dream, you’ve got to grab it and never let go.",
-"Nothing is impossible. The word itself says I’m possible!",
-"There is nothing impossible to they who will try.",
-"The bad news is time flies. The good news is you’re the pilot.",
-"Life has got all those twists and turns. You’ve got to hold on tight and off you go.",
-"Keep your face always toward the sunshine, and shadows will fall behind you.",
-"Be courageous. Challenge orthodoxy. Stand up for what you believe in. When you are in your rocking chair talking to your grandchildren many years from now, be sure you have a good story to tell.",
-"You make a choice: continue living your life feeling muddled in this abyss of self-misunderstanding, or you find your identity independent of it. You draw your own box.",
-"Success is not final, failure is not fatal: it is the courage to continue that counts.",
-"You define your own life. Don’t let other people write your script."];
+let noun1 = ["The pig", "The duck","An emo kid", "Suzy down the street", "Michael Jordan", "Ronald Mcdonald", "Daffy Duck","A baby","The Mailman"]
+let verb = ["ran over","Fell in love with","Married","Killed","fought","cooked dinner for","cleaned up after","read a bedtime story to","fought","kissed"]
+let noun2 = ["Dolly Parton","Miley Cyrus", "Justin Beiber","A priest","A police officer","Some random dude","themself","Patrick Mahomes","Michael Jackson's son Blanket","Minnie Mouse","Shaq","Aunt Jemima","Tom Brady","My wife","your wife","your mom"]
+let why = ["they were angry", "they were jealous", "they didnt like the look they received when they walked in the door","they secretly wanted to marry them","they knew they would never be as good as them","they didnt get their morning coffee","they were on acid and saw their face as a demon","they were posessed by an ancient ghost from the civil war","they felt like it"]
 
-let birthdayPhrases = ["Another year and you are still the MVP, Mom. I hope all your happy birthday wishes come true. Happy birthday!",
-"Glad you made it this far and glad you made me. Happy birthday mommy. I’ll make sure your sweet birthday wishes come true.",
-"Mom, everyone is jealous of how beautiful you look on your birthday. Your birthday wishes should be to keep doing you. Happy birthday mom!",
-"Mom we celebrate you because your accomplishments created our world. Happy birthday!",
-"I am who I am because of you mom. Your happy birthday wishes are my command. Happy birthday!",
-"You’ve always been my favorite person to celebrate, Mom. You are going to get all your birthday wishes today. Happy birthday Mom!",
-"I promise not to let the wax drip onto your cake this time. Just blow the candles out faster and make your birthday wishes. Happy birthday!",
-"I cherish you for being an amazing parent. Have a very happy birthday, mom.",
-"One of these years I might give you a grand child as a birthday gift. Happy birthday!",
-"Mom, you already know how endless my love is for you, so how do I make your birthday wish come true? Happy birthday!"];
+let a = Math.floor(Math.random() * noun1.length)
+let b = Math.floor(Math.random() * verb.length)
+let c = Math.floor(Math.random() * noun2.length)
+let d = Math.floor(Math.random() * why.length)
 
-let z = Math.floor(Math.random() * 2);
-let x = Math.floor(Math.random() * 9 +1);
-
-if(z === 0){
-    document.write("Inspirational: " + inspirationalPhrases[x])
-}else{
-    document.write("Birthday: " + birthdayPhrases[x])
-}
-
-//console.log(z);
+document.write(`${noun1[a]} ${verb[b]} ${noun2[c]} because ${why[d]} `)
+//console.log();
